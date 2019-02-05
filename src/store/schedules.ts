ce267ee3e3a5d18a04d236073b8c9138c62b9161
Yama-Tomo <mazda13bturbo@gmail.com>
2019-02-06 00:40:50 +0900

    feat: for module style store

---
+++ src/store/schedules.ts
@@ -0,0 +1,9 @@
+import state from '~/store_modules/schedules/state';
+import actions from '~/store_modules/schedules/actions';
+import mutations from '~/store_modules/schedules/mutations';
+
+export {
+  state,
+  actions,
+  mutations,
+};
